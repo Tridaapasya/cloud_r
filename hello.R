45efe58bfe864b67f946141d63056bbc35d80f64
Tridaapasya <212201903981@mhs.dinus.ac.id>
2021-08-10 15:14:24 +0700

    tambah umur

--- hello.R
+++ hello.R
@@ -1,9 +1,12 @@
 "Hello selamat datang di dunia pemrograman R"
 
-nama = "Trida Apasya"
-umur = 20
-ipk = 3.76
+nama <- "Trida Apasya"
+umur <- 20
+ipk <- 3.76
 
 print(nama)
 print(umur)
 print(ipk)
+
+umur <- umur + 10
+print(umur)
